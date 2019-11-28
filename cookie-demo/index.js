44526aacbc0b4b072b0452b1dc900dcfdc3ccb34
Anthony Ang <anthonyang25@gmail.com>
2019-11-28 16:33:47 +0800

    Update index.js

--- cookie-demo/index.js
+++ cookie-demo/index.js
@@ -1,4 +1,6 @@
 const app = require('express')();
+const cp = require('cookie-parser');
+app.use(cp());
 
 app.get("/", (req, res) => {
     //session cookie
@@ -25,10 +27,20 @@ app.get("/img", (req, res) => {
     });
 
     //"Mock Hackers steal api/ url"
-    app.get("/steal", (req, res) => {
+app.get("/steal", (req, res) => {
         //in the req obj
         res.send(`Stole your cookies and store them to the DB. Here they are... ${req.query.v}`);
     });
+
+    //Trying cookie-parser; https://www.npmjs.com/package/cookie-parser
+app.get('/set', (req, res) => {
+        res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
+        res.cookie("name", "value", {
+            sameSite: true
+          });
+          res.send(`Here they are... ${req.cookies['rememberme']} ${req.cookies['name']}`);
+          res.end();
+      });
 
 
 app.listen(8080, () => console.log("listening on port 8080"));
