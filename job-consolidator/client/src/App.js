29a753f376403a77856a4f9bd03b9232c7f08e5f
Anthony Ang <anthonyang25@gmail.com>
2019-12-11 15:07:40 +0800

    Update App.js

--- job-consolidator/client/src/App.js
+++ job-consolidator/client/src/App.js
@@ -1,3 +1,5 @@
+
+import { useState, useEffect } from "react";
 import React from 'react';
 import './App.css';
 
@@ -6,27 +8,52 @@ import Jobs from './Jobs';
 const JOB_API_URL = '/api/jobs';
 // const JOB_API_URL = '/api/jobs/StackOverFlow';
 
-async function fetchJobs(updateCb) {
-  const res = await fetch(JOB_API_URL);
-  let json = await res.json();
-  
-  updateCb(json);
-}
+const App = resource => {
+  const [listOfJobs, setJobs] = useState([]);
 
-function App() {
+  const fetchResource = async resource => {
+    const res = await fetch(JOB_API_URL);
+    let json = await res.json();
 
-  const [jobList, updateJobs] = React.useState([]);
+    setJobs(json);
+  };
 
-  //https://reactjs.org/docs/hooks-effect.html
-  React.useEffect(() => {
-    fetchJobs(updateJobs); //fetch jobs and update state after render
-  }, []) //only run effect once
+  // https://reactjs.org/docs/hooks-effect.html
+  // If 'resource' value is same, arrow func is not called.
+  // If diff, will fetch after each re-render
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
 
-  return (
+  //pass in the listOfJobs
+    return (
     <div className="App">
-      <Jobs jobs={jobList} />
+      <Jobs jobs={listOfJobs} />
     </div>
   );
-}
+};
+
+// async function fetchJobs(updateJobs) {
+//   const res = await fetch(JOB_API_URL);
+//   let json = await res.json();
+  
+//   updateJobs(json);
+// }
+
+// function App() {
+
+//   const [jobList, updateJobs] = React.useState([]);
+
+//   //https://reactjs.org/docs/hooks-effect.html
+//   React.useEffect(() => {
+//     fetchJobs(updateJobs); //fetch jobs and update state after render
+//   }, []) //only run effect once
+
+//   return (
+//     <div className="App">
+//       <Jobs jobs={jobList} />
+//     </div>
+//   );
+// }
 
 export default App;
